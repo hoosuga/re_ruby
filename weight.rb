33a0445343fa00c36bd2dc218bdc2c7311e1c5a5
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-19 06:38:17 +0000

    [Add] 練習

--- weight.rb
+++ weight.rb
@@ -6,7 +6,7 @@ puts "#{name}さんの体重は#{weight * 2}kgです"
 puts '#{name}さんの体重は#{weight * 2}kgです'
 
 name = "B"
-weigh = 70
+weight = 70
 
 puts name + "さんの体重は" + weight.to_s + "kgです"
 puts "#{name}さんの体重は#{weight}kgです"
