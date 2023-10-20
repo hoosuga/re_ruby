35ab9a24982ea7b29297f1973bd236f62637720e
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-20 10:38:10 +0000

    [Add] 変数展開

--- weight.rb
+++ weight.rb
@@ -3,3 +3,9 @@ weight = 70
 
 puts name + "さんの体重は" + weight.to_s + "kgです"
 puts "#{name}さんの体重は#{weight}kgです"
+
+name = "A"
+weight = 50
+
+puts name + "さんの体重は" + weight.to_s + "kgです"
+puts "#{name}さんの体重は#{weight}kgです"
