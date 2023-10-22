dce1935d629a5368b20407736ecad6758fc69bb2
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-22 00:33:29 +0000

    [Add] 練習

--- score.rb
+++ score.rb
@@ -6,3 +6,12 @@ end
 if score >= 50 || (score <= 100 && score >= 80)
   puts "スコアが50点以上または、80点以上100点以下"
 end
+
+score = 70
+if (score >= 50 || score <= 100) && score >= 80
+  puts "得点は50以上または100以下、かつ80以上"
+end
+
+if score >= 50 || (score <= 100 && score >= 80)
+  puts "得点は50点以上または80点以上かつ100点以下"
+end
