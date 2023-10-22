f59113cdced4b37cb48b883c8794a49ba73d8a25
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-22 00:36:43 +0000

    [Add] 練習

--- lesson5.rb
+++ lesson5.rb
@@ -1,9 +1,10 @@
-total_price = 120
+total_price = 500
 
 if total_price > 100
-  puts "みかんを購入。所持金に余りあり。"
+  puts "みかんを購入。所持金にあまりあり。"
 elsif total_price == 100
-  puts "みかんを購入。所持金は0円"
+  puts "みかんを購入。所持金は0円。"
 else
   puts "みかんを購入することができません。"
 end
+  
