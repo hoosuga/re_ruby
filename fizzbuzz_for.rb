a6a575e9e270252372947ae8737a1c8898e75580
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-22 06:34:27 +0000

    [Add] 練習

--- fizzbuzz_for.rb
+++ fizzbuzz_for.rb
@@ -1,11 +1,11 @@
-for number in 1..30
-  if number % 15 == 0
+for i in (1..30) do
+  if i % 3 == 0 && i % 5 == 0
     puts "FizzBuzz"
-  elsif number % 3 == 0
+  elsif i % 3 == 0
     puts "Fizz"
-  elsif number % 5 == 0
+  elsif i % 5 == 0
     puts "Buzz"
   else
-    puts number
+    puts i
   end
 end
