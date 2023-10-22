8fe7c5877f65937aa4c629f06f376dc784b29e9d
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-22 01:12:39 +0000

    [Add] 練習

--- amounts.rb
+++ amounts.rb
@@ -2,3 +2,8 @@ amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
 amounts.each do |fruit, amount|
   puts "#{fruit}は#{amount}個です"
 end
+
+amounts = {"ポテト"=>3, "パン"=>5, "ドーナツ"=>8}
+amounts.each do |food, amount|
+  puts "#{food}は#{amount}個です"
+end
