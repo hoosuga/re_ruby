8fdfeb378a5a374c05b783726fcfb4ec0b3e965c
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-22 06:28:01 +0000

    [Add] 演習

--- new.rb
+++ new.rb
@@ -1,5 +1,11 @@
 # インスタンスメソッド
 class Car
+  
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
@@ -9,14 +15,17 @@ class Car
 end
 
 car = Car.new
-car.turn("右")
-car.run(5)
+car.move("右",5)
 
 # クラスメソッド
 class Car
   def self.run(distance)
     puts "車で#{distance}km走ります。"
   end
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
 end
 
 Car.run(10)
+Car.turn("右")
