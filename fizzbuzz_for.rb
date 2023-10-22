1d37815e2a511998d8adfb403ace358a809b73fa
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-22 06:01:49 +0000

    [Add] FizzBuzz if文

---
+++ fizzbuzz_for.rb
@@ -0,0 +1,11 @@
+for number in 1..30
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+end
