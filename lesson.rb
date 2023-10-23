3645cc2fd0519c4fbb05b77db2d82e3e72579e14
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-23 02:05:14 +0000

    [Add] 演習

--- lesson.rb
+++ lesson.rb
@@ -1,16 +1,11 @@
-def fizz_buzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
+for i in (1..30) do
+  if i % 15 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
   else
-    number.to_s
+    puts i
   end
 end
-
-puts "数字を入力してください"
-input = gets.to_i
-puts "結果は..."
-puts fizz_buzz(input)
