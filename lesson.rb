d844dafcf2e8b8ab1e2ed11d66cc9f3e425af4c8
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-23 02:00:13 +0000

    [Add] 演習

--- lesson.rb
+++ lesson.rb
@@ -1,26 +1,16 @@
-puts "計算を始めます"
-puts "何回繰り返しますか？"
-
-input = gets.to_i
-i = 1
-
-while i <= input  do
-  puts "#{i}回目の計算"
-  puts "２つの値を入力してください"
-
-  a = gets.to_i
-  b = gets.to_i
-  
-  puts "a=#{a}"
-  puts "b=#{b}"
-
-  puts "計算結果を出力します"
-  puts "#{a}+#{b}=#{a + b}"
-  puts "#{a}-#{b}=#{a - b}"
-  puts "#{a}*#{b}=#{a * b}"
-  puts "#{a}/#{b}=#{a / b}"
-
-  i += 1
+def fizz_buzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number.to_s
+  end
 end
 
-puts "計算を終了します"
+puts "数字を入力してください"
+input = gets.to_i
+puts "結果は..."
+puts fizz_buzz(input)
