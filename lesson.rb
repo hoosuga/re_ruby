843735f70cec0cd5f7f06b883e4946653fa40881
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-23 01:16:41 +0000

    [Add] 練習用

--- lesson.rb
+++ lesson.rb
@@ -1,27 +1,9 @@
-#each文
+total_price = 130
 
-(1..30).each do |i|
-  if i % 15 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  else
-    puts i
-  end
-end
-
-#for文
-
-for i in (1..30) do
-  if i % 3 == 0 && i % 5 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  else
-    puts i
-  end
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円。"
+else
+  puts "みかんを購入することができません。"
 end
