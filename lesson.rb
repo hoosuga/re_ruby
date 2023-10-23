55e218da1809ca4d99970ce069beedc01b43fc87
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-23 02:26:44 +0000

    [Add] 練習

--- lesson.rb
+++ lesson.rb
@@ -2,21 +2,10 @@ class Car
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
-  
-  def trun(direction)
-    puts "#{direction}に曲がります。"
-  end
 end
 
-car = Car.new
-car.trun("右")
-car.run(5)
-
-class Car
-  def self.trun(direction)
-    puts "#{direction}に曲がります。"
-  end
+class Bus < Car
 end
-   
 
-Car.trun("右")
+bus = Bus.new
+bus.run(5)
