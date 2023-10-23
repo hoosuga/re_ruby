605e6cbcee9a284dd2a44f75f02304baa84027b5
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-23 02:09:24 +0000

    [Add] 練習

--- lesson.rb
+++ lesson.rb
@@ -1,11 +1,13 @@
-for i in (1..30) do
-  if i % 15 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  else
-    puts i
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+  
+  def turn(direction)
+    puts "#{direction}に曲がります。"
   end
 end
+
+car = Car.new
+car.turn("右")
+car.run(5)
